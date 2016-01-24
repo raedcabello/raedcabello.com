bc10971e9d66d1dd7bd90571a52de2dc0f760eb9
Keith Bartholomew <keith.bartholomew@rackspace.com>
2016-01-23 20:15:56 -0600

    static markup for the individual work pages

--- _src/js/site.js
+++ _src/js/site.js
@@ -10,3 +10,8 @@ if(document.getElementById('contact-form')) {
   var ContactForm = require('./components/contact-form');
   ReactDOM.render(<ContactForm />, document.getElementById('contact-form'));
 }
+
+
+if(document.body.clientHeight > window.innerHeight) {
+  document.body.classList.remove('fixed-footer');
+}
